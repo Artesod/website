42145643e5977996302a4bed387b68002415f5dd
Joshua <joshua.canta@stonybrook.edu>
2021-02-25 13:00:15 -0500

    Updates

---
+++ precache-manifest.4d20370c2caa399365fec300f261bf02.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f01a161f653ac433a0bcd540a13a2",
+    "url": "/master/index.html"
+  },
+  {
+    "revision": "4e1dc1088d49566bcc75",
+    "url": "/master/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "80bf61e26e032596e8ab",
+    "url": "/master/static/css/main.ccbf6e6b.chunk.css"
+  },
+  {
+    "revision": "4e1dc1088d49566bcc75",
+    "url": "/master/static/js/2.afec1868.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/master/static/js/2.afec1868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bf61e26e032596e8ab",
+    "url": "/master/static/js/main.21c97df0.chunk.js"
+  },
+  {
+    "revision": "b16481319b197ed95377",
+    "url": "/master/static/js/runtime-main.c2917818.js"
+  },
+  {
+    "revision": "8901184ecd077029b7da32c3d206836d",
+    "url": "/master/static/media/night.8901184e.jpg"
+  },
+  {
+    "revision": "28944e01125f88db3a1f70223b8fcfdd",
+    "url": "/master/static/media/night2.28944e01.jpg"
+  },
+  {
+    "revision": "b23a349b16398d930aa847c0e11271c3",
+    "url": "/master/static/media/night3.b23a349b.jpg"
+  },
+  {
+    "revision": "9420e9056bd4befa164f7a81ede430d5",
+    "url": "/master/static/media/sky.9420e905.jpg"
+  },
+  {
+    "revision": "5eafbde8a427dc968914d5c2282bcfa6",
+    "url": "/master/static/media/sky2.5eafbde8.jpg"
+  },
+  {
+    "revision": "4f80ea9acfaf1676aac2ac4fc83fdab0",
+    "url": "/master/static/media/sky3.4f80ea9a.jpg"
+  }
+]);
